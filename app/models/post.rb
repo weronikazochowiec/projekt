db12316a3e395528d0d6097f7786477331722837
weronikazochowiec <veronica.zochowiec@gmail.com>
2020-05-25 14:44:05 +0200

    dodanie asocjacji i kategorii

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user_blog
   belongs_to :user
+  belongs_to :location
+  has_many :comments
+  has_many :post_categories
+  has_many :categories, :through => :post_categories
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :post_categories
+  resources :categories
   resources :locations
   resources :comments
   resources :posts
