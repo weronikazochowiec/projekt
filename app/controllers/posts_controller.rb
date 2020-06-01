7cae775ad0a0df9f5899283eadf82d3fdcb07b76
weronikazochowiec <veronica.zochowiec@gmail.com>
2020-06-01 16:52:13 +0200

    możliwość wyszukania postów z danej kategorii

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,11 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    if params[:category_ids]
+      @posts = Post.joins(:categories).where('categories.id' =>  params[:category_ids])
+    else
+      @posts = Post.all
+    end
   end
 
   # GET /posts/1
