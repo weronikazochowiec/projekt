81a5788aba98c7bec4739fe6affd8ba78c07cac9
weronikazochowiec <veronica.zochowiec@gmail.com>
2020-06-04 19:01:18 +0200

    dodanie uzytkownika admina

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,14 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    if user&.admin?
-     can :access, :rails_admin       # only allow admin users to access Rails Admin
-      can :dashboard
-      can :manage, :all
-    else
-      can :read, :all                   # allow everyone to read everything
-    end
-  end
+  #def initialize(user)
+  #  user ||= User.new # guest user (not logged in)
+  #  if user&.admin?
+  #   can :access, :rails_admin       # only allow admin users to access Rails Admin
+  #    can :dashboard
+  #    can :manage, :all
+  #  else
+  #    can :read, :all                   # allow everyone to read everything
+  #  end
+  #end
 end
